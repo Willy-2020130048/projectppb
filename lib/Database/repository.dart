2cdfe869b11da8ce2eec4200850cdce7e86abc33
vnkpanka <ceciliangelaivanka17@gmail.com>
2023-12-10 01:06:31 +0700

    Tambah Cart Page

--- lib/Database/repository.dart
+++ lib/Database/repository.dart
@@ -1,46 +1,99 @@
-import 'package:projectppb/Models/produks.dart';
+import '../Models/produks.dart';
 
 class RepositoryProduk {
   List<Produk> list = [
     Produk(
         gambar: 'images/Hutao.jpg',
-        nama: 'Costume Full Hu Tao',
+        nama: 'Costume Hu Tao',
         harga: 150000,
         jenis: 'Costume',
         keterangan:
             "Penyewaan costume Hu Tao, termasuk baju, celana, sepatu, topi dan wig. Penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
-        ),
+        jumlah: 0,
+        total: 0),
     Produk(
-        gambar: 'images/Hutao.jpg',
-        nama: 'Costume Hu Tao',
-        harga: 150000,
+        gambar: 'images/Zerotwo.jpg',
+        nama: 'Costume Zero Two',
+        harga: 100000,
         jenis: 'Costume',
         keterangan:
-            "Penyewaan costume Hu Tao, termasuk baju, celana, sepatu, topi dan wig. Penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
-        ),
-        Produk(
-        gambar: 'images/Hutao.jpg',
-        nama: 'Costume Hu Tao',
-        harga: 150000,
+            "Penyewaan costume Zero Two, termasuk baju, celana dan wig. Penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
+        jumlah: 0,
+        total: 0),
+    Produk(
+        gambar: 'images/Gura.jpg',
+        nama: 'Costume Gura',
+        harga: 100000,
         jenis: 'Costume',
         keterangan:
-            "Penyewaan costume Hu Tao, termasuk baju, celana, sepatu, topi dan wig. Penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
-        ),
-        Produk(
-        gambar: 'images/Hutao.jpg',
-        nama: 'Costume Hu Tao',
-        harga: 150000,
-        jenis: 'Costume',
+            "Penyewaan costume Gura, termasuk hoodie dan wig. Penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
+        jumlah: 0,
+        total: 0),
+    Produk(
+        gambar: 'images/WigKazuha.jpeg',
+        nama: 'Wig Kazuha',
+        harga: 50000,
+        jenis: 'Wig',
         keterangan:
-            "Penyewaan costume Hu Tao, termasuk baju, celana, sepatu, topi dan wig. Penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
-        ),
-        Produk(
-        gambar: 'images/Hutao.jpg',
-        nama: 'Costume Hu Tao',
-        harga: 150000,
-        jenis: 'Costume',
+            "Penyewaan wig Kazuha, penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
+        jumlah: 0,
+        total: 0),
+    Produk(
+        gambar: 'images/WigMaki.jpeg',
+        nama: 'Wig Maki',
+        harga: 50000,
+        jenis: 'Wig',
         keterangan:
-            "Penyewaan costume Hu Tao, termasuk baju, celana, sepatu, topi dan wig. Penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
-        ),
+            "Penyewaan wig Maki, penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
+        jumlah: 0,
+        total: 0),
+    Produk(
+        gambar: 'images/WigNeonGenesis.jpeg',
+        nama: 'Wig Neon Genesis',
+        harga: 50000,
+        jenis: 'Wig',
+        keterangan:
+            "Penyewaan wig Neon Genesis, penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
+        jumlah: 0,
+        total: 0),
+    Produk(
+        gambar: 'images/AccAOT.jpg',
+        nama: 'Senjata Sword Gun',
+        harga: 65000,
+        jenis: 'Acc',
+        keterangan:
+            "Penyewaan senjata AOT, senjata terbuat dari alumunium. Penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
+        jumlah: 0,
+        total: 0),
+    Produk(
+        gambar: 'images/AccSenjata.jpg',
+        nama: 'Staff of Homa',
+        harga: 65000,
+        jenis: 'Acc',
+        keterangan:
+            "Penyewaan Staff of Homa dari genshin impact. Penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
+        jumlah: 0,
+        total: 0),
+    Produk(
+        gambar: 'images/AccTopi.jpg',
+        nama: 'Topi One Piece',
+        harga: 50000,
+        jenis: 'Acc',
+        keterangan:
+            "Penyewaan topi bajak laut luffy terbuat dari jerami. Penyewaan dengan durasi 3 hari dan harus langsung dikembalikan atau dikenakan denda 50.000",
+        jumlah: 0,
+        total: 0),
   ];
+
+  List<Produk> getListByJenis(String jenis) {
+    List<Produk> temp = [];
+    for (var produk in list) {
+      if (jenis == "All") {
+        temp.addAll(list);
+      } else if (produk.jenis == jenis) {
+        temp.add(produk);
+      }
+    }
+    return temp;
+  }
 }
---
+++ lib/Screens/Pembeli/profile_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          width: double.infinity,
+          color: Colors.red,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: const [
+                CircleAvatar(),
+                Padding(
+                  padding: EdgeInsets.only(left: 16),
+                  child: Text(
+                    "Nama Profile",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: const [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Akun",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: Text("Username"),
+                  trailing: Text("Email"),
+                ),
+                ListTile(
+                  title: Text("Nama"),
+                  trailing: Text("Email"),
+                ),
+                ListTile(
+                  title: Text("Nomor Telp"),
+                  trailing: Text("Email"),
+                ),
+                ListTile(
+                  title: Text("Alamat"),
+                  trailing: Text("Email"),
+                ),
+                ListTile(
+                  title: Text("Email"),
+                  trailing: Text("Email"),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: const [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Transaksi",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: Text("History Pembelian"),
+                  trailing: Icon(Icons.arrow_right),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
